a8091887705959a5b849a153d50a033d651dcbe0
Wesley Miranda <wesleyrod.br@gmail.com>
2026-03-01 15:00:46 -0300

    feat(back): add CORS in project

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+  app.enableCors({
+    origin: '*', // Mudar para a URL do Front em prod.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
@@ -22,9 +29,19 @@ async function bootstrap() {
     ) 
     .build();
 
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }));
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  logger.log(`API rodando em: http://localhost:${port}/api`);
+  logger.log(`Documentação em: http://localhost:${port}/docs`);
 }
 bootstrap();
