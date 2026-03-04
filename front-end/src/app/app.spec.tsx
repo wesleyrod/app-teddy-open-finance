688ddfb390f6ae0fb472196c30fd0145cdab6ed2
Wesley Miranda <wesleyrod.br@gmail.com>
2026-03-04 15:40:33 -0300

    fix(front): fix tests after broker in pipeline

--- front-end/src/app/app.spec.tsx
+++ front-end/src/app/app.spec.tsx
@@ -1,26 +1,15 @@
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { baseElement } = render(<App />);
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
-    const { getAllByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(
-      getAllByText(new RegExp('Welcome @org/front-end', 'gi')).length > 0
-    ).toBeTruthy();
+  it('should render the login page by default', () => {
+    const { baseElement } = render(<App />);
+    // Unauthenticated users are redirected to login
+    expect(baseElement.querySelector('form')).toBeTruthy();
   });
 });
